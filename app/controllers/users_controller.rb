5e8359de405dc0ebeec666b043b7952b48e64252
JZims <zimmerman.j@hotmail.com>
2021-10-07 23:41:10 -0400

    adding user creattion/validation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,27 @@
 class UsersController < ApplicationController
+
+    def index
+        @user = User.all
+    end
+
+    def new
+        @user = User.new(user_params)
+
+        if @user.save
+            redirect_to user_path(@user)
+        else 
+            render :new
+        end
+    end
+
+    def show
+        @user = User.find(user_params[:name])
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:name, :password)
+    end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,16 @@
 class User < ApplicationRecord
+    has_secure_password 
+
+    has_many :adoptions
+    has_many :pets, through: :adoptions
+    has_many :inventories
+    has_many :items, through: :inventories
+
+    validates :name, presence: true
+    validates :name, uniqueness: true
+    validates :password, length: {minimum: 8}
+
+    
+
+
 end
